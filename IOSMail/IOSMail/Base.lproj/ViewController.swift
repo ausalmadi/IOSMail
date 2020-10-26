9da86be6eed26a212b8fe7f75d9302ef1e90ff9d
Gervan Nantel <gervann@yahoo.ca>
2020-10-26 19:57:53 -0400

    fixes

---
+++ IOSMail/IOSMail/Base.lproj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSMail
+//
+//  Created by student on 2020-10-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
