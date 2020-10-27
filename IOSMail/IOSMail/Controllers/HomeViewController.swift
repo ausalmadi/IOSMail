d10a84a22041f037e0e0a385c1a67a2102f251de
student <aos.abdulhafith@students.williscollege.com>
2020-10-27 15:54:25 -0400

    FIXES

---
+++ IOSMail/IOSMail/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  iosMail
+//
+//  Created by student on 2020-10-27.
+//
+
+import UIKit
+
+class HomeViewController: MainViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
