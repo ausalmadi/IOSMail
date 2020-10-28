c3cb3d8852cfddef79af0b40268db19dc20789a0
lalaantika sharma <sharmal0707@gmail.com>
2020-10-28 13:03:52 -0400

    table view

--- IOSMail/IOSMail/Controllers/SentViewController.swift
+++ IOSMail/IOSMail/Controllers/SentViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class SentViewController: MainViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data = ["To: ABC", "To: CDF","To: GBHDG","To: ABC", "To: CDF","To: GBHDG"]
+    let date = ["1Aug","12Aug","16Aug","17Aug","20Aug","1Sep"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
         // Do any additional setup after loading the view.
+        self.tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
     
 
@@ -25,5 +33,23 @@ class SentViewController: MainViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+//MARK: Tableview delegate and datasource
+
+extension SentViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier:"cell", for: indexPath) as? TableViewCell {
+            cell.tableLabel.text = self.data[indexPath.row]
+            cell.tableDateLabel.text = self.date[indexPath.row]
+            return cell
+        }
+        return UITableViewCell()
+    }
 
 }
