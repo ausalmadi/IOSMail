26323a52585f028af4b8e934426db529463b1960
Gervan Nantel <gervann@yahoo.ca>
2020-11-16 13:49:38 -0500

    Updates

--- IOSMail/IOSMail/AppDelegate.swift
+++ IOSMail/IOSMail/AppDelegate.swift
@@ -27,13 +27,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 			return
 		}
 		// Perform any operations on signed in user here.
-		let userId = user.userID                  // For client-side use only!
-		let idToken = user.authentication.idToken // Safe to send to the server
+//		let userId = user.userID                  // For client-side use only!
+//		let idToken = user.authentication.idToken // Safe to send to the server
 		let fullName = user.profile.name
-		let givenName = user.profile.givenName
-		let familyName = user.profile.familyName
-		let email = user.profile.email
-		// [START_EXCLUDE]
+//		let givenName = user.profile.givenName
+//		let familyName = user.profile.familyName
+//		let email = user.profile.email
+//		// [START_EXCLUDE]
 		NotificationCenter.default.post(
 			name: Notification.Name(rawValue: "ToggleAuthUINotification"),
 			object: nil,
