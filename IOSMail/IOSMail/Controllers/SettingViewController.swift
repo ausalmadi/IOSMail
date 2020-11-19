f3bd8a0e277ee807d358375cfacb4f5110fdfac4
Gervan Nantel <gervann@yahoo.ca>
2020-11-19 16:46:41 -0500

    Able to save settings to the database

--- IOSMail/IOSMail/Controllers/SettingViewController.swift
+++ IOSMail/IOSMail/Controllers/SettingViewController.swift
@@ -6,38 +6,58 @@
 //
 
 import UIKit
+import SwiftUI
 import RealmSwift
 
 class SettingViewController: MainViewController {
     
-//    let realm = try! Realm()
-    var settings = Settings()
+    let realm = RealmService.shared.realm
+    var settings: Results<Settings>?
+    
 
     @IBOutlet weak var emailAddress: UITextField!
     @IBOutlet weak var signatureTextView: UITextView!
+    @IBOutlet weak var stateSwitch: UISwitch!
+    @IBOutlet weak var addSigLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        emailAddress.text = settings.emailReply
-        signatureTextView.text = settings.signatureLine
-
+        
+        settings = realm.objects(Settings.self)
+        print(Settings)
+        
+        
+     
         
     }
     
     @IBAction func signatureOn(_ sender: UISwitch) {
-//        UISwitch.init()
+        
+        if stateSwitch.isOn {
+            addSigLabel.text = "Add Signature to eMail"
+            stateSwitch.setOn(true, animated:true)
+
+        } else {
+            addSigLabel.text = "Don't Add Signature to Email"
+            stateSwitch.setOn(false, animated:true)
+
+        }
+        
         
     }
     
     @IBAction func saveButtonPressed(_ sender: UIButton) {
         
-        settings.emailReply = emailAddress.text!
-        settings.signatureLine = signatureTextView.text!
+        let newSettings = Settings()
+        newSettings.emailReply = emailAddress.text!
+        newSettings.dateUpdated = Date()
+        newSettings.signatureLine = signatureTextView.text!
+        newSettings.useSignature = stateSwitch.isOn
         
-
+        RealmService.shared.create(newSettings)
+        
+        dismiss(animated: true, completion: nil)
         }
-        
-    
-    
+
 }
