a68325a7fe210cc990e14e566f42cd84694574b6
burningman2k20 <60669658+burningman2k20@users.noreply.github.com>
2020-11-21 21:22:05 -0500

    Update HomeViewController.swift

--- IOSMail/IOSMail/Controllers/HomeViewController.swift
+++ IOSMail/IOSMail/Controllers/HomeViewController.swift
@@ -16,8 +16,7 @@ class HomeViewController: UIViewController {
 	var messages = [MailData]()
 	let gmailService = GTLRGmailService.init()
 	var messageList = [GTLRGmail_Message]()
-	//let mailManager = MailManager.shared
-
+	
     @IBOutlet weak var checkBox: UIButton!
     @IBOutlet weak var deletePressed: UIButton!
     
@@ -60,10 +59,7 @@ class HomeViewController: UIViewController {
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
-
-
 		inboxTitle.text? = inboxText
-        
     }
     
 	@IBAction func signout(_ sender: Any) {
@@ -73,7 +69,6 @@ class HomeViewController: UIViewController {
 			mvc.modalPresentationStyle = .fullScreen
 			self.present(mvc, animated: true, completion: nil)
 		}
-
 	}
 
 	deinit {
@@ -103,8 +98,6 @@ class HomeViewController: UIViewController {
 
 			if notification.userInfo != nil {
 				guard let userInfo = notification.userInfo as? [String:String] else { return }
-				//inboxTitle.text = userInfo["statusText"]!
-
 			}
 		}
 	}
@@ -125,8 +118,6 @@ class HomeViewController: UIViewController {
 
 		gmailService.executeQuery(listQuery) { (ticket, response, error) in
 	if response != nil {
-	//                print("Response: ")
-	//                print(response)
 	self.getFirstMessageIdFromMessages(response: response as! GTLRGmail_ListMessagesResponse)
 	} else {
 	print("Error: ")
@@ -141,54 +132,38 @@ class HomeViewController: UIViewController {
 		var date : String = ""
 		var subject : String = ""
 	let messagesResponse = response as GTLRGmail_ListMessagesResponse
-	//print("Latest Message: ")
-	//print(messagesResponse.messages!.count as Any)
-//	do {
-//		try
-			messagesResponse.messages!.forEach({ (msg) in
+		
+		messagesResponse.messages!.forEach({ (msg) in
 		let query = GTLRGmailQuery_UsersMessagesGet.query(withUserId: "me", identifier: msg.identifier!)
 		gmailService.executeQuery(query) { [self] (ticket, response, error) in
 
 		if response != nil {
-	//                        print(response)
 			self.messageList.append(response as! GTLRGmail_Message)
-			//print("Message: ")
 		self.messageList.forEach { (message) in
 	//get the body of the email and decode it
 		message.payload!.headers?.forEach {( head) in
 
 		if head.name=="Date" {
-//			print(self.base64urlToBase64(base64url: head.name ?? "default name"))
-//			print(self.base64urlToBase64(base64url: head.value ?? "default value"))
 			date = self.base64urlToBase64(base64url: head.value ?? "default value")
 		}
 		if head.name=="Subject" {
-//			print(self.base64urlToBase64(base64url: head.name ?? "default name"))
-//			print(self.base64urlToBase64(base64url: head.value ?? "default value"))
 			subject = self.base64urlToBase64(base64url: head.value ?? "default value")
 		}
 		if head.name=="From" {
-//			print(self.base64urlToBase64(base64url: head.name ?? "default name"))
-//			print(self.base64urlToBase64(base64url: head.value ?? "default value"))
 			from = self.base64urlToBase64(base64url: head.value ?? "default value")
 		}
 		if head.name=="To" {
-//			print(self.base64urlToBase64(base64url: head.name ?? "default name"))
-//			print(self.base64urlToBase64(base64url: head.value ?? "default value"))
 			to = self.base64urlToBase64(base64url: head.value ?? "default value")
 		}
 	}
-		//print(message.payload!.jsonString())
 
 	guard let message2 = message.payload!.parts?[0] else
 	{return }
 	let mail = self.base64urlToBase64(base64url: message2.body!.data!)
-	
 
 	if let data = Data(base64Encoded: mail) {
 	let m = MailData(subject: subject, from: from, to: to, body:String(data: data, encoding: .utf8)!, date: date)
 	self.messages.append(m)
-	//print(String(data: data, encoding: .utf8)!)
 	}
 	}
 
@@ -200,9 +175,6 @@ class HomeViewController: UIViewController {
 	}
 
 	})
-//	} catch{
-//	print("error \(error)")
-//	}
 
 }
 
@@ -217,25 +189,20 @@ func base64urlToBase64(base64url: String) -> String {
 	}
 }
 
-
 //MARK: Tableview delegate and datasource
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
-
-
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return self.messages.count
     }
 
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		//let selectedTrail = trails[indexPath.row]
 		print(indexPath.row)
 		index = indexPath.row
 		performSegue(withIdentifier: "MainToReader", sender: self)
 
 	}
-
 
     internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier:"cell", for: indexPath) as? TableViewCell {
@@ -247,7 +214,5 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
         }
         return UITableViewCell()
     }
-
-
 }
 
