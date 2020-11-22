f51c15af832d2a72b3a2a5c5d8288793766fe898
David Crispin <ignite01@hotmail.com>
2020-11-22 08:39:47 -0500

    if message data nil, crashes. need to fix this

--- IOSMail/IOSMail/Controllers/HomeViewController.swift
+++ IOSMail/IOSMail/Controllers/HomeViewController.swift
@@ -109,7 +109,7 @@ class HomeViewController: UIViewController {
 
 		gmailService.authorizer = authorizer
 	//gmailService.shouldFetchNextPages = true
-		listQuery.maxResults = 5
+		listQuery.maxResults = 1
 
 		gmailService.executeQuery(listQuery) { (ticket, response, error) in
 	if response != nil {
