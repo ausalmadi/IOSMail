bb2e2fbc9b3695bf260f22eda953ae27d7653809
Gervan Nantel <gervann@yahoo.ca>
2020-11-30 16:32:30 -0500

    Added model fields for the team

--- IOSMail/IOSMail/Models/mailData.swift
+++ IOSMail/IOSMail/Models/mailData.swift
@@ -25,12 +25,19 @@ struct MailData {
 	}
 }
 
-class newMailData: Object {
-	@objc dynamic var mailSubject: String = ""
+class EmailData: Object {
+    @objc dynamic var messageID: String = ""
+    @objc dynamic var mailBox: String? = "" // INBOX, SENT, DRAFT
+    @objc dynamic var emailDate: Date?
+	@objc dynamic var emailSubject: String = ""
+    @objc dynamic var emailBody: String?
+    @objc dynamic var emailReply: String?
+    @objc dynamic var emailCrUp: Date?
 	@objc dynamic var fromSender: String = ""
 	@objc dynamic var toRecepiant: String = ""
-	@objc dynamic var emailBody: String?
-	@objc dynamic var emailDate: Date?
-	//@objc dynamic var emailTime: String?
+    @objc dynamic var ccRecepiant: String? = ""
+    @objc dynamic var bccRecepiant: String? = ""
 	@objc dynamic var sendDate: Date = Date()
+    @objc dynamic var emailRead: Bool = false
+    @objc dynamic var emailSent: Bool = false
 }
