57f74e3e37d079ba9aef96cc5f055370366e2c77
student <aos.abdulhafith@students.williscollege.com>
2020-12-02 17:11:15 -0500

    home view loading data

--- IOSMail/IOSMail/Controllers/HomeViewController.swift
+++ IOSMail/IOSMail/Controllers/HomeViewController.swift
@@ -32,19 +32,20 @@ class HomeViewController: UIViewController {
 	@IBOutlet var inbox: UITableView!
 	@IBOutlet weak var sb: UISearchBar!
 
-	override func viewWillAppear(_ animated: Bool) {
-	}
-
+//	override func viewWillAppear(_ animated: Bool) {
+//	}
+    let realm = RealmService.shared.realm
+       var mail: Results<EmailData>?
     override func viewDidLoad() {
 
 		super.viewDidLoad()
-
+        mail = realm.objects(EmailData.self)
         manager.listInboxMessages(tableview: tableView, folder: manager.mailBox)
 
-		NotificationCenter.default.addObserver(self,
-           selector: #selector(MainViewController.receiveToggleAuthUINotification(_:)),
-           name: NSNotification.Name(rawValue: "ToggleAuthUINotification"),
-           object: nil)
+//		NotificationCenter.default.addObserver(self,
+//           selector: #selector(MainViewController.receiveToggleAuthUINotification(_:)),
+//           name: NSNotification.Name(rawValue: "ToggleAuthUINotification"),
+//           object: nil)
 
         self.tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
         self.tableView.delegate = self
@@ -103,23 +104,70 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return self.manager.messages.count
+	//	return self.manager.messages.count
+       return mail?.count ?? 1
+        
     }
-
-	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		print(indexPath.row)
-		index = indexPath.row
-		performSegue(withIdentifier: "MainToReader", sender: self)
-	}
-
-    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier:"cell", for: indexPath) as? TableViewCell {
-			cell.tableLabel.text = self.manager.messages[indexPath.row].subject
-			cell.tableDateLabel.text = self.manager.messages[indexPath.row].date
-			cell.tableSubjectLabel.text = self.manager.messages[indexPath.row].body
-
-            return cell
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if   editingStyle == .delete {
+                guard let message = mail?[indexPath.row] else { return  }
+                RealmService.shared.delete(message)
+            }
+            tableView.reloadData()
+            tableView.deselectRow(at: indexPath, animated: true)
+            
         }
-        return UITableViewCell()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell" , for: indexPath)
+        if let massage = mail?[indexPath.row]{
+           // print("\(massage.emailSubject)")
+            //cell.textLabel?.text = massage.
+//            cell.tableLabel.text = massage.emailSubject
+//            cell.tableDateLabel.text = massage.emailDate
+//            cell.tableSubjectLabel.text = massage.emailBody
+        }else{
+            print("error")
+        }
+        return cell
     }
+
+
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//            let  cell = tableview.dequeueReusableCell(withIdentifier: "email", for: indexPath)
+//            if let massage = mail?[indexPath.row]{
+//                cell.textLabel?.text = massage.massege
+//            }else{
+//                print("no messages ")
+//            }
+//             return cell
+//        }
+
+    
+//	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+////		print(indexPath.row)
+////		index = indexPath.row
+////		performSegue(withIdentifier: "MainToReader", sender: self)
+//	}
+
+//    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let  cell = tableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//               if let massage = mail?[indexPath.row]{
+//                   cell.textLabel?.text = massage.massege
+//               }else{
+//                   print("no messages ")
+//               }
+//                return cell
+//           }
+
+//        if let cell = tableView.dequeueReusableCell(withIdentifier:"cell", for: indexPath) as? TableViewCell {
+//            let emailData = EmailData()
+//
+//            cell.tableLabel.text = emailData.emailSubject
+//			cell.tableDateLabel.text = emailData.emailDate
+//            cell.tableSubjectLabel.text = emailData.emailBody
+//
+//            return cell
+//        }
+//        return UITableViewCell()
 }
+    
